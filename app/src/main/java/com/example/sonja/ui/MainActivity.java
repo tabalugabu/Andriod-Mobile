9488b8a5bd055a2da4cfe7b7104697fbfc03bdf5
sonni93 <38693058+sonni93@users.noreply.github.com>
2018-05-09 14:20:34 +0200

    test123

--- app/src/main/java/com/example/sonja/ui/MainActivity.java
+++ app/src/main/java/com/example/sonja/ui/MainActivity.java
@@ -31,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
+
+
+
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
