e03d469492267d4e379c59b6ea27e2ba33669a38
sonni93 <38693058+sonni93@users.noreply.github.com>
2018-05-11 16:19:49 +0200

    test123

--- app/src/main/java/com/example/sonja/ui/MainActivity.java
+++ app/src/main/java/com/example/sonja/ui/MainActivity.java
@@ -41,5 +41,5 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
+//Test3
 }
