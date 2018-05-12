9d55211cf9e648bf35bbfae6a64cd4d736abece2
sonni93 <38693058+sonni93@users.noreply.github.com>
2018-05-12 14:14:49 +0200

    Splash + first screen_4

--- app/src/main/java/com/example/sonja/ui/MainActivity.java
+++ app/src/main/java/com/example/sonja/ui/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.sonja.ui;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btn_logIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_logIn = (Button) findViewById(R.id.btn_login);
+        btn_logIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //ruft Screen2 auf
+        Intent intent = new Intent(this, Screen2.class);
+        startActivity(intent);
+        this.finish();
     }
 }
