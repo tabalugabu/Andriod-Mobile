091dda0e4f636f7d12f788fa156f9da0719fafa5
sonni93 <38693058+sonni93@users.noreply.github.com>
2018-05-13 11:43:56 +0200

    Bottom Menu Bar in screen2

--- app/src/main/java/com/example/sonja/ui/Screen2.java
+++ app/src/main/java/com/example/sonja/ui/Screen2.java
@@ -1,7 +1,15 @@
 package com.example.sonja.ui;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
 
 public class Screen2 extends AppCompatActivity {
 
@@ -9,5 +17,6 @@ public class Screen2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
+
     }
 }
